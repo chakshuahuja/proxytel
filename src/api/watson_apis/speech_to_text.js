2ee14f7db6fa8da3ddcfc39de094502380555dca
Chakshu Ahuja <ahuja.chaks@gmail.com>
2017-09-16 23:08:27 -0700

    Added dummy .wav and text files for apis testing

--- src/api/watson_apis/speech_to_text.js
+++ src/api/watson_apis/speech_to_text.js
@@ -1,27 +1,29 @@
-var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
-var fs = require('fs');
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+const fs = require('fs');
 
-var speech_to_text = new SpeechToTextV1 ({
+const speech_to_text = new SpeechToTextV1({
   username: '3ba5be6a-4d35-41d0-ae22-0d99b697df63',
-  password: 'RZPH8uWZiSBd'
+  password: 'RZPH8uWZiSBd',
 });
 
-var params = {
+const params = {
   model: 'en-US_BroadbandModel',
   content_type: 'audio/wav',
-  'interim_results': true,
-  'max_alternatives': 3,
-  'word_confidence': false,
+  interim_results: true,
+  max_alternatives: 3,
+  word_confidence: false,
   timestamps: false,
   keywords: ['colorado', 'tornado', 'tornadoes'],
-  'keywords_threshold': 0.5
+  keywords_threshold: 0.5,
 };
 
 // Create the stream.
-var recognizeStream = speech_to_text.createRecognizeStream(params);
+const recognizeStream = speech_to_text.createRecognizeStream(params);
 
 // Pipe in the audio.
-fs.createReadStream('../../../test/resources/angry_sample_audio.wav').pipe(recognizeStream);
+fs
+  .createReadStream('../../../test/resources/angry_sample_audio.wav')
+  .pipe(recognizeStream);
 
 // Pipe out the transcription to a file.
 recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
@@ -31,7 +33,9 @@ recognizeStream.setEncoding('utf8');
 
 // // Listen for events.
 // recognizeStream.on('results', function(event) { onEvent('Results:', event); });
-recognizeStream.on('data', function(event) { onEvent('Data:', event); });
+recognizeStream.on('data', event => {
+  onEvent('Data:', event);
+});
 // recognizeStream.on('error', function(event) { onEvent('Error:', event); });
 // recognizeStream.on('close', function(event) { onEvent('Close:', event); });
 // recognizeStream.on('speaker_labels', function(event) { onEvent('Speaker_Labels:', event); });
@@ -39,5 +43,5 @@ recognizeStream.on('data', function(event) { onEvent('Data:', event); });
 // Displays events on the console.
 function onEvent(name, event) {
   // if(name == "Data:")
-    console.log(JSON.stringify(event, null, 2));
-};
+  console.log(JSON.stringify(event, null, 2));
+}
--- src/api/watson_apis/tone_analyzer.js
+++ src/api/watson_apis/tone_analyzer.js
@@ -1,17 +1,14 @@
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-var tone_analyzer = new ToneAnalyzerV3({
+const tone_analyzer = new ToneAnalyzerV3({
   username: '4465d93b-14a5-4340-a2cd-67cb939899f3',
   password: 'gXcd0JWgXvsR',
-  version_date: '2016-05-19'
+  version_date: '2016-05-19',
 });
 
-var params = require('./../../../test/resources/tone-example.json');
+const params = require('./../../../test/resources/tone-example.json');
 
-tone_analyzer.tone(params, function(error, response) {
-  if (error)
-    console.log('error:', error);
-  else
-    console.log(JSON.stringify(response, null, 2));
-  }
-);
+tone_analyzer.tone(params, (error, response) => {
+  if (error) console.log('error:', error);
+  else console.log(JSON.stringify(response, null, 2));
+});
